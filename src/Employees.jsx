1bfb953984fb82db45c5fb08d9910a7125a7ccf5
Valik94 <34817962+valik94@users.noreply.github.com>
2023-02-04 20:37:21 +0000

    using jsx xml styling we inline style the employee cards and create a dropdown list using select tags and option tags

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -92,20 +92,34 @@ const Employees = () => {
 
   return (
      <main className = "container">
-       <div className="row">
+      <div className="row justify-content-center mt-3 mb-3"> 
+        <div class = "col-6">
+          <select>
+            <option value="TeamA">Team A</option>
+            <option value="TeamB">Team B</option>
+            <option value="TeamC">Team C</option>
+            <option value="TeamD">Team D</option>
+          </select>
+        </div>
+      </div>
+          <div className="row justify-content-center mt-3 mb-3"> 
         <div class = "col-8">
+          <div className="card-collection">
        {
       employees.map((employee) => (
-        <div id={employee.id} className = "card">
+        <div id={employee.id} className = "card m-2" style={{cursor: "pointer" }}>
         <p>{employee.fullName}</p>
-        <img src={femaleProfile} className="card-img-top"/>
-        <div className="card-body">
+        
+          {(employee.gender === 'male')?<img src={maleProfile} className="card-img-top"/>
+                                        :<img src={femaleProfile} className="card-img-top"/>}
+          <div className="card-body">
           <h5 className="card-title">Full Name: {employee.fullName}</h5>
           <p className="card-text"><b>Desigation:</b> {employee.designation}</p>
         </div>
         </div>
       ))
        }
+            </div>
           </div>
          </div>
      </main>
