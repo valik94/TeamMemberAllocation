35f5d940d18bde6163e2f88fe69e8d260e210ad5
Valik94 <34817962+valik94@users.noreply.github.com>
2023-02-04 17:34:48 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <main>
+      <Header/>
+      <Content/>
+      <Footer/>  
+    </main>
+  );
+}
+
+export default App;
