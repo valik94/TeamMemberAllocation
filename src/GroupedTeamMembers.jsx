3b23b5cb907a5f707213f34e1434b4a54a30b2bf
Valik94 <34817962+valik94@users.noreply.github.com>
2023-02-10 18:54:11 +0000

    implemented the logic for groupTeamMembers function. The function returns an array of teams all of which contain 3 properties in each team object. including teamName, members and whether the team is collapsed or expanded using tertiary Boolean operator

--- src/GroupedTeamMembers.jsx
+++ src/GroupedTeamMembers.jsx
@@ -3,13 +3,29 @@ import {useState} from 'react';
 
 const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) =>{
   //pass in the functiongroupTeamMembers to the useState hook that will return default value of grouped employees array. Each object in group employees array will contain property that contains teamName, employee data for employees and property named collapsed. For collapsed property we wills tore state property in collapsible list that will retunr from group tea members component.
-  const [groupedEmployees, setGroupedData]  = useState(groupTeamMembers());
+  const [groupedEmployees, setGroupedData]  = useState(groupTeamMembers()); //returns default state stored object in employees array
   function groupTeamMembers()
   {
 //the collapsed property of the relevant team will be stored in the current boolean value of collapsed property
 //either collapse or expand the Team listing
+     var teams = [];
+    var teamAMembers = employees.filter((employee) => employee.teamName === "TeamA");
+    var teamA = {team:'teamA', members:teamAMembers, collapsed: selectedTeam === 'TeamA'?false:true}
+    teams.push(teamA);
 
-    
+    var teamBMembers = employees.filter((employee) => employee.teamName === "TeamB");
+    var teamB = {team:'teamB', members:teamAMembers, collapsed: selectedTeam === 'TeamB'?false:true}
+    teams.push(teamB);
+
+    var teamCMembers = employees.filter((employee) => employee.teamName === "TeamC");
+    var teamC = {team:'teamC', members:teamAMembers, collapsed: selectedTeam === 'TeamC'?false:true}
+    teams.push(teamC);
+
+    var teamDMembers = employees.filter((employee) => employee.teamName === "TeamD");
+    var teamD = {team:'teamD', members:teamAMembers, collapsed: selectedTeam === 'TeamD'?false:true}
+    teams.push(teamD);
+
+    return teams;
     
   }
   return (
